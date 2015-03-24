e80b2f09f55606194f082125dd6f8df70ed48d83
w3Q <email.takac@gmail.com>
2015-03-25 00:10:01 +0100

    Init of blog repo

---
+++ src/BlogBundle/Entity/Post.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Post
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="published", type="integer")
+     */
+    private $published;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="urlTitle", type="string", length=255)
+     */
+    private $urlTitle;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set published
+     *
+     * @param integer $published
+     * @return Post
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return integer 
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Set urlTitle
+     *
+     * @param string $urlTitle
+     * @return Post
+     */
+    public function setUrlTitle($urlTitle)
+    {
+        $this->urlTitle = $urlTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get urlTitle
+     *
+     * @return string 
+     */
+    public function getUrlTitle()
+    {
+        return $this->urlTitle;
+    }
+}
